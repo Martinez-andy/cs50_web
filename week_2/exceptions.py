265e55942230d0162ebb92c8986bd883306b5d79
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2023-12-23 08:05:15 -0500

    Added week2 files

--- week_2/exceptions.py
+++ week_2/exceptions.py
@@ -1,6 +1,14 @@
-x = int(input('x: '))
-y = int(input('y: '))
+import sys
+try:
+    x = int(input('x: '))
+    y = int(input('y: '))
+except ValueError:
+    print('Inputs must be ints or floats')
+    sys.exit(1)
 
-result = x / y
-
-print(f'{x} / {y} = {result}')
+try:
+    result = x / y
+    print(f'{x} / {y} = {result}')
+except ZeroDivisionError:
+    print('Cannot divide by zero')
+    sys.exit(1)

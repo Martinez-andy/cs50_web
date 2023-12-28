474fc88900fc7902dc3e8e7984985fbd196f7b7c
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2023-12-28 07:45:55 -0500

    Restarting lecture 3

--- week_3/lecture3/lecture3/settings.py
+++ week_3/lecture3/lecture3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&l^udfl)%s5=(b!b=q3sj$su7(2z+7=$!#9vnmpig=h)fy1$&9'
+SECRET_KEY = 'django-insecure-^=r_)h5@56(ny&@g=jy9ptgpv0@-ii(=#hrkxr(hc++1es!y=+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'hello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

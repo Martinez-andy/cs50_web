884b4291c76dd929bd21fd2cb4af9d3c27db4937
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2023-12-29 08:41:28 -0500

    First bullet point done

--- psets/wiki/encyclopedia/views.py
+++ psets/wiki/encyclopedia/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
+
+def mdToHtml(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    return markdowner.convert(content)
 
 
 def index(request):
@@ -8,3 +16,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wikiPage(request, title):
+    content = mdToHtml(title)
+    if content == None:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title,
+        "content": content
+    })

b6b570fa94eccd31b7f974aed3cf3bb57c175c5e
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-07 14:31:45 -0500

    Weird bug. Unsure how to proceed. Continue video at 1:01:17

--- week_4/airline/flights/models.py
+++ week_4/airline/flights/models.py
@@ -2,8 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-# Create a class for each table | This is similar to initializing a new SQLite table
+# Create a class (or a model) for each table 
+# These models are useful for when editing an SQLite data base
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+    
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="deoartures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
